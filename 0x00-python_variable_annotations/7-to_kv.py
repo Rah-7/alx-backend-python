f91e025f4d214cb7fe4a719adca3238d2763710a
Rah-7 <rahigirmam16@gmail.com>
2023-10-05 19:04:25 +0300

    edit file

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-"""a type-annotated function to_kv that takes a
-string k and an int OR float v as arguments and
-returns a tuple."""
-import typing
+'''Task 7's module.
+'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
